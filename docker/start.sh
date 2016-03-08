c38d79928c77cec15943f6483c75d576ae0bfe7c
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-03-08 14:14:35 -0500

    Add some tweaks to docker

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,13 @@
 cd /usr/src
 
 if [ -z "$staging" ]; then
+  echo "Checking out master branch";
+  git checkout master
+else
   echo "Checking out develop branch";
   git checkout develop
 fi
 
 git pull
-npm install
+npm install --unsafe-perm
 npm run start
