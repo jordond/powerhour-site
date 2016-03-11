1a8e54525ac390acc1a3c750bd0c17510570a0d9
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-03-11 18:08:59 -0500

    Remove old phone component
    Incorporate it into header.component

--- src/client/ui/ui.module.js
+++ src/client/ui/ui.module.js
@@ -3,7 +3,6 @@ import angular from 'angular';
 import core from '../core/core.module';
 
 import header from './header/header.component';
-import phone from './phone/phone.component';
 import highlights from './highlights/highlights.component';
 
 const dependencies = [
@@ -14,7 +13,6 @@ const mod =
   angular
     .module('app.ui', dependencies)
     .component('header', header)
-    .component('phone', phone)
     .component('highlights', highlights);
 
 export default mod.name;
