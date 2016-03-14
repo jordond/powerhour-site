24098ab836feb923b4c884421284ad5ee29c811a
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-03-14 15:27:24 -0400

    Move scss out of core module

--- src/client/core/core.module.js
+++ src/client/core/core.module.js
@@ -3,7 +3,6 @@ import angular from 'angular';
 import 'font-awesome/css/font-awesome.css';
 import 'skeleton-css/css/skeleton.css';
 import 'normalize.css/normalize';
-import './core';
 
 // Core vendor libs
 import ngAnimate from 'angular-animate';
